cf968a5d55ff16928d182b3348937fea48e479ab
2101031000204-Margi <2101031000204@silveroakuni.ac.in>
2024-03-01 14:08:21 +0530

    n

--- src/app/chat-home/chat-home.component.ts
+++ src/app/chat-home/chat-home.component.ts
@@ -68,6 +68,7 @@ export class ChatHomeComponent implements OnInit, OnDestroy{
     
     this.receiverSubscription = interval(4000).subscribe(() => {
       this.Receivermsg();
+   
     });
   
   }
