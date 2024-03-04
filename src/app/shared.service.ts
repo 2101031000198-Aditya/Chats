84c5447b1295eb7d2a8223fe6ac4bbff5dfacb51
2101031000204-Margi <2101031000204@silveroakuni.ac.in>
2024-03-04 11:44:04 +0530

    image-component

--- src/app/shared.service.ts
+++ src/app/shared.service.ts
@@ -5,6 +5,8 @@ import { Injectable } from '@angular/core';
 })
 export class SharedService {
   // sharedProperty: string = '';
+  width: number = 200;
+  height: number = 200;
   coordinates: { lat: number, lon: number } = { lat: 0, lon: 0 };
   Username: string = '';
 
